b236ceccb7ef1d4a77a36960f2894153e1650b91
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-08-18 21:07:28 +0200

    Adds send-sms-messages (patch)

---
+++ utils/send-sms-messages.js
@@ -0,0 +1,36 @@
+(async () => {
+  require('dotenv').config()
+  const logger = require('../lib/logger')
+  const getMobilePhoneNumber = require('../lib/get-mobile-number')
+  const sendSmsMessage = require('../lib/send-sms')
+  const avtaler = require('../data/avtaler-query.json')
+  let successCount = 0
+  let failCount = 0
+  logger('info', ['utils', 'send-sms-message', 'ready', `got ${avtaler.length} avtaler`])
+  while (avtaler.length > 0) {
+    const avtale = avtaler.pop()
+    logger('info', ['utils', 'send-sms-message', 'avtale', avtale.aid])
+    const mobileNumber = await getMobilePhoneNumber(avtale.uid)
+    if (mobileNumber) {
+      const message = {
+        sender: 'VgsTelemark',
+        receivers: [mobileNumber],
+        message: `Husk å signere avtalen om ElevPC https://svarut.ks.no/forsendelse/${avtale.fid}/1/signering Velkommen til skolestart.`,
+        operation: 9
+      }
+      const result = await sendSmsMessage(message)
+      if (result) {
+        logger('info', ['utils', 'send-sms-message', 'avtale', avtale.aid, 'message sendt'])
+        successCount++
+      } else {
+        logger('info', ['utils', 'send-sms-message', 'avtale', avtale.aid, 'message not sendt'])
+        failCount++
+      }
+    } else {
+      failCount++
+      logger('info', ['utils', 'send-sms-message', 'avtale', avtale.aid, 'mobile number not available'])
+    }
+    logger('info', ['utils', 'send-sms-message', `${avtaler.length} avtaler to go`])
+  }
+  logger('info', ['utils', 'send-sms-message', 'finished', 'success', successCount, 'fails', failCount])
+})()
